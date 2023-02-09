25a9b65a56b8610f823c76e85f5d7d49dd6892d9
Ryan Lee <ryanl3708@gmail.com>
2023-02-09 21:00:20 +0000

    updating styling and scripts

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ function getName(){
   let userName = prompt("what is your name?");
   alert(`Welcome ${userName}! This is Ryan's About Me introduction page!`);
   alert('Let\'s play a guessing game! Please answer with a (yes/y or no/n)!');
+  return userName;
 }
 // 5 prompt yes no questions
 
@@ -52,11 +53,15 @@ let myNum = 7;
 let chance = 4;
 let questionSix = prompt('What number (1~20) am I thinking of?');
 let guess = parseInt(questionSix);
-while(guess !== myNum && chance > 0){
+while(guess !== myNum && chance > 1){
   if(guess > myNum){
     alert('Your guess is too high!');
-  } else{
+  } else if(guess < myNum){
     alert('Your guess is too low!');
+  } else if(guess === myNum){
+    alert('Great! You are correct!');
+  } else {
+    alert('What is that input??')
   }
   chance -= 1;
   questionSix = prompt(`what number (1~20) am I thinking of? (You have ${chance} chances!`);
@@ -64,7 +69,31 @@ while(guess !== myNum && chance > 0){
 }
 
 
+let myfruitlist = ['mango', 'apple', 'peach', 'grape', 'cherry'];
+let score = 0;
 
+for(let i = 0; i < 6; i++){
+  let questionSeven = prompt('guess one of my favorite fruits (type in singular!)').toLowerCase();
+  for(let j=0; j < myfruitlist.length; j++){
+    if (questionSeven === myfruitlist(j)) {
+      alert('You guessed correctly!');
+      score += 1;
+    } else {
+      alert('Wrong. Guess again!');
+    }
+  }
+
+}
+
+function fruitanswers(){
+  let text = 'You got ' + score + ' of my fruits correctly! Here is the list of fruits I like:\n';
+  for (let i = 0; i < myfruitlist.length; i++){
+    text += myfruitlist[i] + '\n';
+  }
+  return text;
+}
+
+alert(fruitanswers());
 // final message
 alert(`thank you for playin ${userName}`);
 
